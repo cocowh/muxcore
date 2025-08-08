8b305e2721401b102d2d1bc50423fbf6e359bc09
cocowh <cocowh@qq.com>
2025-08-08 18:29:32 +0800

    bugfix

--- core/ws/client.go
+++ core/ws/client.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
 package ws
 
 import (
